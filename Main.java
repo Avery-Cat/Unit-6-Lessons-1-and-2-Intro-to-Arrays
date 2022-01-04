816a1753cbde6c7447c9387c9a24db40bd0950b1
Avery Andrews <56173421+Avery-Cat@users.noreply.github.com>
2022-01-04 14:20:41 +0000

    changed several things (did in class)

--- Main.java
+++ Main.java
@@ -35,7 +35,7 @@ class Main {
       System.out.println("nums2[" + i + "] = " + nums2[i]);
     }
 
-    String [] candy = new String [5];
+    /*String [] candy = new String [5];
     Scanner it = new Scanner(System.in);
     int index = 0;
 
@@ -44,7 +44,68 @@ class Main {
       candy [index] = it.nextLine();
       System.out.println("candy[" + index + "] = " + candy[index]);
       index++;
+    }*/
+
+    //Lesson 2 searching
+    //when looking for something, iterate
+    int look = 12;
+    int indexWhere = -1;
+    for(int index = 0; index<nums.length; index++){
+      if(nums[index] == look){
+        indexWhere = index;
+        break; //forces you to exit the loop
+      }
+    }
+    if(indexWhere != -1){
+      System.out.println("Look has been found at " + indexWhere);
+    }else{
+      System.out.println("Look has not been found.");
     }
 
+    //searching for a String
+    /*String [] words = {"have","a","nice","day"};*/
+    String words[] = {"have","a","nice","day"}; //not wrong
+    int indexWhere2 = -1;
+    for(int index = 0; index<words.length; index++){
+      if(words[index].equals("nice")){
+        indexWhere2 = index;
+        break; //forces you to exit the loop
+      }
+    }
+    if(indexWhere2 != -1){
+      System.out.println("word has been found at " + indexWhere2);
+    }else{
+      System.out.println("word has not been found.");
+    }
+
+    //count values in an array
+    int evens = 0;
+    int odds = 0;
+    for(int index = 0; index<nums.length; index++){
+      if((nums[index] %2) == 0){
+        evens++;
+      }else{
+        odds++;
+      }
+    }
+    System.out.println("Evens = "+evens+" Odds = "+odds);
+
+    //set flag if word has more than 4 letters
+    boolean flag = false;
+    for(int index = 0; index<words.length; index++){
+      //  String    how many char
+      if(words[index].length() >= 4){
+        flag = true;
+        break;
+      }
+    }
+    System.out.println("Were there any words with more than 4 letters? " + flag);
+
+    String [] words2 = {"apple", "banana", "peach", "cherries", "blueberries"};
+    for(int i = 0; i<words2.length; i++){
+      String first = words2[i].substring(0,1);
+      String last = words2[i].substring(words2[i].length() - 1);
+      System.out.println("Index Value " + i + ": " + words2[i] + " " + first + " " + last);
+    }
   }
 }
